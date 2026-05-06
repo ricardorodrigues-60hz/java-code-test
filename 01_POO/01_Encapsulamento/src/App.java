d179c7adf13cd8296cd50ef50ad1c7df66eb94c9
Ricardo Rodrigues <101021873+ricardorodrigues-60hz@users.noreply.github.com>
2026-05-06 17:14:45 -0300

    Second Commit

---
+++ 01_POO/01_Encapsulamento/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.abrirMenu();
+        
+
+
+
+    }
+}
